3c344d9aefc670146a27f6aa6d1f1074ffa10e03
kacey_c <kaceycreative@gmail.com>
2021-01-09 18:34:50 -0500

    Add footer icons and Cast Studies page

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying cast studies archive
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="site-content">
+	<div class="main-content" role="main">
+        <?php while ( have_posts() ) : the_post();
+            $image_1 = get_field("image_1");
+            $size = "full";
+            $services = get_field('services'); 
+        ?>   
+	<article class ="case-study-archives"> 
+    	<aside class="case-study-sidebar">
+			<h2><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<h4><?php echo $services; ?></h4>
+
+			<?php the_excerpt(); ?>
+			<p class="read-more-link"><a href="<?php echo the_permalink(); ?>">View Project</a></p>
+		</aside>
+		<div class="case-study-img">
+			<?php if($image_1) { 
+				echo wp_get_attachment_image( $image_1, $size );
+			} ?>
+        </div>
+    </article>
+
+		<?php endwhile; // end of the loop. ?>
+	</div><!-- .main-content -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
